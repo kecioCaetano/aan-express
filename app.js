795037af7b8ed998a023099ee3a92de7c6562c40
Kecio Caetano Barbosa <kecio.kecio@gmail.com>
2026-03-04 05:30:21 -0500

    views

--- app.js
+++ app.js
@@ -3,7 +3,25 @@ const app = express()
 const authorRouter = require('./routes/authorRouter')
 const bookRouter = require('./routes/bookRouter')
 const indexRouter = require('./routes/indexRouter')
+const path = require("node:path")
 
+const links = [
+    {href: '/', text: "Home"},
+    {href: 'about', text: "About"},
+]
+
+const users = ['Rose', 'Cake', 'Biff']
+
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
+
+app.get('/', (req, res) => {
+    res.render('index', {links: links, users: users})
+})
+
+app.get('/', (req, res) =>{
+    res.render('index',{message: 'EJS rocks!'})
+})
 
 app.use('/authors',authorRouter)
 app.use('/books', bookRouter)
